a04c64c77891ea63748df17db930029e53a58805
Tyler Arehart <tarehart@gmail.com>
2018-01-28 23:34:04 -0800

    Converting GrpcServer and some UI stuff to Kotlin.

---
+++ java/src/tarehart/rlbot/GrpcService.kt
@@ -0,0 +1,59 @@
+package tarehart.rlbot
+
+import io.grpc.stub.StreamObserver
+import rlbot.api.BotGrpc
+import rlbot.api.GameData
+import tarehart.rlbot.input.Chronometer
+import tarehart.rlbot.input.SpinTracker
+import tarehart.rlbot.ui.StatusSummary
+import java.util.*
+
+class GrpcService(private val statusSummary: StatusSummary) : BotGrpc.BotImplBase() {
+    private val bots = HashMap<Int, Bot>()
+    private val chronometer = Chronometer()
+    private val spinTracker = SpinTracker()
+
+    private var frameCount: Long = 0
+
+    override fun getControllerState(request: GameData.GameTickPacket, responseObserver: StreamObserver<GameData.ControllerState>) {
+        responseObserver.onNext(doGetControllerState(request))
+        responseObserver.onCompleted()
+    }
+
+    private fun doGetControllerState(request: GameData.GameTickPacket): GameData.ControllerState {
+
+
+        try {
+            val playerIndex = request.playerIndex
+
+            // Do nothing if we know nothing about our car
+            if (request.playersCount <= playerIndex) {
+                return AgentOutput().toControllerState()
+            }
+
+            val translatedInput = AgentInput(request, playerIndex, chronometer, spinTracker, frameCount++)
+
+            val bot = bots.getOrPut(playerIndex, { initNewBot(translatedInput, playerIndex) })
+
+            return bot.processInput(translatedInput).toControllerState()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return AgentOutput().toControllerState()
+        }
+
+    }
+
+    private fun initNewBot(translatedInput: AgentInput, playerIndex: Int) : Bot {
+        val newBot: Bot
+        if (translatedInput.myCarData.name.startsWith("JumpingBean")) {
+            newBot = JumpingBeanBot(translatedInput.team, playerIndex)
+        } else if (translatedInput.myCarData.name.startsWith("Pain")) {
+            newBot = DemolitionBot(translatedInput.team, playerIndex)
+        } else {
+            newBot = ReliefBot(translatedInput.team, playerIndex)
+        }
+        statusSummary.markTeamRunning(translatedInput.team, playerIndex, newBot.debugWindow)
+
+        return newBot
+    }
+}
