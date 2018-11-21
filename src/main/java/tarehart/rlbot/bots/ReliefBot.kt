306bd5e494d93e226a23811a909b6b64002f9a69
Joshua Lloyd <j.nevercast@gmail.com>
2018-11-21 21:37:16 +1300

    Migrate ReliefBot to TacticalBot

--- src/main/java/tarehart/rlbot/bots/ReliefBot.kt
+++ src/main/java/tarehart/rlbot/bots/ReliefBot.kt
@@ -2,66 +2,40 @@ package tarehart.rlbot.bots
 
 import tarehart.rlbot.AgentInput
 import tarehart.rlbot.AgentOutput
+import tarehart.rlbot.TacticalBundle
 import tarehart.rlbot.physics.ArenaModel
 import tarehart.rlbot.planning.Plan
 import tarehart.rlbot.planning.SteerUtil
 import tarehart.rlbot.tactics.*
 
-class ReliefBot(team: Team, playerIndex: Int) : BaseBot(team, playerIndex) {
+class ReliefBot(team: Team, playerIndex: Int) : TacticalBot(team, playerIndex) {
 
-    private var tacticsAdvisor: TacticsAdvisor? = null
-
-    private fun gameMode() : GameMode {
+    override fun getNewTacticsAdvisor(tacticsAdvisor: TacticsAdvisor?): TacticsAdvisor? {
         val gameMode = GameModeSniffer.getGameMode()
 
-        if (tacticsAdvisor == null || !tacticsAdvisor!!.suitableGameModes().contains(gameMode)) {
+        if (tacticsAdvisor == null || !tacticsAdvisor.suitableGameModes().contains(gameMode)) {
             if (gameMode == GameMode.SOCCER) {
-                tacticsAdvisor = SoccerTacticsAdvisor()
                 println("Game Mode: Soccar")
+                return SoccerTacticsAdvisor()
             } else if (gameMode == GameMode.DROPSHOT) {
                 println("Game Mode: Dropshot")
-                tacticsAdvisor = DropshotTacticsAdvisor()
+                return DropshotTacticsAdvisor()
             } else if (gameMode == GameMode.HOOPS) {
                 println("Game Mode: Hoops")
-                tacticsAdvisor = HoopsTacticsAdvisor()
+                return HoopsTacticsAdvisor()
             }
         }
-        return gameMode
+        return null
     }
 
-    override fun getOutput(bundle: TacticalBundle): AgentOutput {
-
-        gameMode()
-
-        val car = input.myCarData
-        val ballPath = ArenaModel.predictBallPath(input)
-        val situation = tacticsAdvisor!!.assessSituation(input, ballPath, currentPlan)
-
-        tacticsAdvisor!!.findMoreUrgentPlan(input, situation, currentPlan)?.let {
-            currentPlan = it
-        }
-
-        if (Plan.activePlanKt(currentPlan) == null) {
-            currentPlan = tacticsAdvisor!!.makeFreshPlan(input, situation)
-            //currentPlan = Plan().withStep(FlexibleKickStep(KickAtEnemyGoal()))
-        }
-
-        currentPlan?.let {
-            if (it.isComplete()) {
-                currentPlan = null
-            } else {
-                it.getOutput(input)?.let { return it }
-            }
-        }
-
-        return SteerUtil.steerTowardGroundPositionGreedily(car, input.ballPosition.flatten()).withBoost(false)
-    }
-
+    // TODO: Make a better implementation for this
+    /*
     override fun roundInLimbo(bundle: TacticalBundle) {
         // NeverCast here, I want my hoops code to run even when the game hasn't started.
-        if (gameMode() == GameMode.HOOPS) {
+        if (GameModeSniffer.getGameMode() == GameMode.HOOPS) {
             val ballPath = ArenaModel.predictBallPath(input)
             tacticsAdvisor!!.assessSituation(input, ballPath, null)
         }
     }
+    */
 }
