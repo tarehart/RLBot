9020a6a6db0c7aff0261e2fffb592eec0f8a4749
Joshua Lloyd <j.nevercast@gmail.com>
2018-11-21 21:53:08 +1300

    ZonePlan also cannot take Bundle.

--- src/main/java/tarehart/rlbot/planning/ZonePlan.kt
+++ src/main/java/tarehart/rlbot/planning/ZonePlan.kt
@@ -6,7 +6,7 @@ import tarehart.rlbot.input.CarData
 import tarehart.rlbot.math.vector.Vector3
 import tarehart.rlbot.tuning.BotLog
 
-class ZonePlan(bundle: TacticalBundle) {
+class ZonePlan(input: AgentInput) {
 
     val ballPosition: Vector3 = input.ballPosition
     val myCar: CarData = input.myCarData
