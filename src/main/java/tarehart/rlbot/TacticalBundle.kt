219abc7b20c9ca0d2ca073bfd9c0037a6a13898b
Joshua Lloyd <j.nevercast@gmail.com>
2018-11-21 21:53:53 +1300

    Create TacticalBundle dummy factory to satisfy plans that now require a TacticalBundle. I'm not sure of the best way to handle this.

--- src/main/java/tarehart/rlbot/TacticalBundle.kt
+++ src/main/java/tarehart/rlbot/TacticalBundle.kt
@@ -4,4 +4,10 @@ import tarehart.rlbot.planning.TeamPlan
 import tarehart.rlbot.planning.ZonePlan
 import tarehart.rlbot.tactics.TacticalSituation
 
-class TacticalBundle(val agentInput: AgentInput, val tacticalSituation: TacticalSituation, val teamPlan: TeamPlan, val zonePlan: ZonePlan)
+class TacticalBundle(val agentInput: AgentInput, val tacticalSituation: TacticalSituation, val teamPlan: TeamPlan, val zonePlan: ZonePlan) {
+    companion object {
+        fun dummy(input: AgentInput): TacticalBundle {
+            return TacticalBundle(input, TacticalSituation.DUMMY, TeamPlan(input), ZonePlan(input))
+        }
+    }
+}
