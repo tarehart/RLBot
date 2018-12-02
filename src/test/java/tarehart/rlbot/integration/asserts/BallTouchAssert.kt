f1aa1a2822f5bb80cf70bb2268f6c27d98c978af
Joshua Lloyd <j.nevercast@gmail.com>
2018-12-02 18:21:15 +1300

    Fix 0ms asserts on ball touch.

--- src/test/java/tarehart/rlbot/integration/asserts/BallTouchAssert.kt
+++ src/test/java/tarehart/rlbot/integration/asserts/BallTouchAssert.kt
@@ -8,13 +8,21 @@ import tarehart.rlbot.math.Plane
 import tarehart.rlbot.math.VectorUtil
 import tarehart.rlbot.planning.GoalUtil
 import tarehart.rlbot.time.Duration
+import tarehart.rlbot.time.GameTime
 
 class BallTouchAssert(timeLimit: Duration): PacketAssert(timeLimit, false) {
+
+    lateinit var firstCheckTime: GameTime
+
     override fun checkBundle(bundle: TacticalBundle, previousBundle: TacticalBundle?) {
+
+        if (! ::firstCheckTime.isInitialized) { // I actually want start time, any reason that the asserts don't get that?
+            firstCheckTime = bundle.agentInput.time
+        }
 
         val touch = bundle.agentInput.latestBallTouch
 
-        if (touch?.playerIndex == bundle.agentInput.playerIndex) {
+        if (touch?.playerIndex == bundle.agentInput.playerIndex && touch?.time > firstCheckTime) {
             status = AssertStatus.SUCCEEDED
         }
     }
