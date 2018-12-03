2212cc09379a0ea508b32ae10fdce6ebbea2949c
Joshua Lloyd <j.nevercast@gmail.com>
2018-12-03 20:28:14 +1300

    Update touch assertion.

--- src/test/java/tarehart/rlbot/integration/asserts/BallTouchAssert.kt
+++ src/test/java/tarehart/rlbot/integration/asserts/BallTouchAssert.kt
@@ -1,5 +1,10 @@
 package tarehart.rlbot.integration.asserts
 
+import rlbot.cppinterop.RLBotDll
+import rlbot.gamestate.BallState
+import rlbot.gamestate.DesiredVector3
+import rlbot.gamestate.GameState
+import rlbot.gamestate.PhysicsState
 import tarehart.rlbot.TacticalBundle
 import tarehart.rlbot.physics.BallPhysics
 import tarehart.rlbot.time.Duration
@@ -26,6 +31,7 @@ class BallTouchAssert(timeLimit: Duration): PacketAssert(timeLimit, false) {
             val car_after = bundle.agentInput.myCarData.velocity
             var ball_after = bundle.agentInput.ballVelocity
 
+
             val impulse = BallPhysics.calculateScriptBallImpactForce(
                     previousBundle.agentInput.myCarData.position,
                     previousBundle.agentInput.myCarData.velocity,
@@ -33,7 +39,17 @@ class BallTouchAssert(timeLimit: Duration): PacketAssert(timeLimit, false) {
                     previousBundle.agentInput.ballVelocity,
                     previousBundle.agentInput.myCarData.orientation.noseVector)
 
+            val physicsImpulse = BallPhysics.calculateCarBallCollisionImpulse(
+                    previousBundle.agentInput.myCarData.position,
+                    previousBundle.agentInput.myCarData.velocity,
+                    previousBundle.agentInput.ballPosition,
+                    previousBundle.agentInput.ballVelocity
+                    )
+
             ball_after -= impulse
+            println("Ball velocity after, without script: ${ball_after}")
+            val delta_before = car_before - ball_before
+            val delta_after = car_after - ball_after
 
             // ball_mass = car_mass * (car_vel_after - car_vel_before) / (ball_vel_after - ball_vel_before)
             val ball_mass = ((car_after - car_before) / (ball_after - ball_before)) * CAR_MASS
@@ -41,7 +57,18 @@ class BallTouchAssert(timeLimit: Duration): PacketAssert(timeLimit, false) {
             println("Car rotation before: ${previousBundle.agentInput.myCarData.spin.angularVelGlobal}")
             println("Car rotation after: ${bundle.agentInput.myCarData.spin.angularVelGlobal}")
 
-            println("Ball Impulse: ${impulse}")
+            println("Ball Script Delta V: ${impulse}")
+            println("Physical Impulse: ${physicsImpulse}")
+            println("Ball physical change in velocity: ${physicsImpulse / 30.0}")
+            println("Car physical change in velocity: ${physicsImpulse / 180.0}")
+            println("Ball change in velocity: ${bundle.agentInput.ballVelocity - previousBundle.agentInput.ballVelocity}")
+
+            println("Relative Velocity; before: ${delta_before}, after: ${delta_after}")
+            println("CoR: ${delta_after / delta_before}")
+
+            println("-----------------")
+            println("Predicted Car final velocity: ${car_before + physicsImpulse / -180.0}")
+            println("Predicted Ball final velocity: ${ball_before + physicsImpulse / 30.0 + impulse}")
             status = AssertStatus.SUCCEEDED
         }
 
